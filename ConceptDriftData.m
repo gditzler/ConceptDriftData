85b08eda2e2c37abe029d274a300162f475a0e2b
Gregory Ditzler <gregory.ditzler@gmail.com>
2013-05-10 15:51:58 -0400

    corrected small error in ConceptDriftData.m. It was a clerical error not a functional error.

--- ConceptDriftData.m
+++ ConceptDriftData.m
@@ -256,3 +256,4 @@ d_transformed = [d(:,1)*cos(alpha)-d(:,2)*sin(alpha), ...
     d(:,1)*sin(alpha)+d(:,2)*cos(alpha)];
 s = ceil(d_transformed(:,1)/a)+floor(d_transformed(:,2)/a);
 labd = 2-mod(s,2);
+end
